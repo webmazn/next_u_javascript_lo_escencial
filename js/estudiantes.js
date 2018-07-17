9ee6a942bd401b55f187149ad9b73b1796928db1
Maycol Zambrano <maycol_630@hotmail.com>
2018-07-17 16:34:14 -0500

    Subiendo index actualizado

--- js/estudiantes.js
+++ js/estudiantes.js
@@ -1,117 +1,192 @@
-var estuadiantes = [
-    {
-        "codigo" : "EST0001",
-        "nombre" : "Juan Carlos Perez",
-        "nota" : 15
-    },
-    {
-        "codigo" : "EST0002",
-        "nombre" : "Maria Luisa Cuculiza",
-        "nota" : 18
-    },
-    {
-        "codigo" : "EST0003",
-        "nombre" : "Ricaldo Salmon Foley",
-        "nota" : 20
-    },
-    {
-        "codigo" : "EST0004",
-        "nombre" : "Duilio Jaime Vega",
-        "nota" : 11
-    },
-    {
-        "codigo" : "EST0005",
-        "nombre" : "Omar Rodriguez Tapia",
-        "nota" : 17
-    },
-    {
-        "codigo" : "EST0006",
-        "nombre" : "Ivan Potrras Cruz",
-        "nota" : 14
-    },
-    {
-        "codigo" : "EST0007",
-        "nombre" : "Ricardo More Gallegos",
-        "nota" : 18
-    },
-    {
-        "codigo" : "EST0008",
-        "nombre" : "Julio Paretto Sanchez",
-        "nota" : 12
-    },
-    {
-        "codigo" : "EST0009",
-        "nombre" : "Jenny Bazan Robles",
-        "nota" : 15
-    },
-    {
-        "codigo" : "EST0010",
-        "nombre" : "Gerson Obregon Leiva",
-        "nota" : 19
-    },
-    {
-        "codigo" : "EST0011",
-        "nombre" : "Eddie Santiago Ruiz",
-        "nota" : 16
-    },
-    {
-        "codigo" : "EST0012",
-        "nombre" : "Gustavo Mego Diaz",
-        "nota" : 13
-    },
-    {
-        "codigo" : "EST0013",
-        "nombre" : "Erika Delgado Torres",
-        "nota" : 18
-    },
-    {
-        "codigo" : "EST0014",
-        "nombre" : "Andrea Torres Iglesias",
-        "nota" : 14
-    },
-    {
-        "codigo" : "EST0015",
-        "nombre" : "Rosa Del Carpio Flores",
-        "nota" : 12
-    },
-    {
-        "codigo" : "EST0016",
-        "nombre" : "Silvana Diaz Osores",
-        "nota" : 11
-    },
-    {
-        "codigo" : "EST0017",
-        "nombre" : "Ana Maria Praderas del Rio",
-        "nota" : 19
-    },
-    {
-        "codigo" : "EST0018",
-        "nombre" : "Juan Pablo Genoves Solaris",
-        "nota" : 13
-    },
-    {
-        "codigo" : "EST0019",
-        "nombre" : "Jorge Frias Malpartida",
-        "nota" : 12
-    },
-    {
-        "codigo" : "EST0020",
-        "nombre" : "Elizabeth Puente Ramos",
-        "nota" : 18
-    },
-    {
-        "codigo" : "EST0021",
-        "nombre" : "Iris Anicama Zamora",
-        "nota" : 12
-    },
-    {
-        "codigo" : "EST0022",
-        "nombre" : "Eulalia Pascual Ubillus",
-        "nota" : 19
-    },
-    {
-        "codigo" : "EST0023",
-        "nombre" : "Dante Sanchez Egocheaga",
-        "nota" : 18
+var objeto = { 
+    "estudiantes" : [
+        {
+            "codigo" : "EST0001",
+            "nombre" : "Juan Carlos Perez",
+            "nota" : 15
+        },
+        {
+            "codigo" : "EST0002",
+            "nombre" : "Maria Luisa Cuculiza",
+            "nota" : 18
+        },
+        {
+            "codigo" : "EST0003",
+            "nombre" : "Ricaldo Salmon Foley",
+            "nota" : 20
+        },
+        {
+            "codigo" : "EST0004",
+            "nombre" : "Duilio Jaime Vega",
+            "nota" : 11
+        },
+        {
+            "codigo" : "EST0005",
+            "nombre" : "Omar Rodriguez Tapia",
+            "nota" : 17
+        },
+        {
+            "codigo" : "EST0006",
+            "nombre" : "Ivan Potrras Cruz",
+            "nota" : 14
+        },
+        {
+            "codigo" : "EST0007",
+            "nombre" : "Ricardo More Gallegos",
+            "nota" : 18
+        },
+        {
+            "codigo" : "EST0008",
+            "nombre" : "Julio Paretto Sanchez",
+            "nota" : 12
+        },
+        {
+            "codigo" : "EST0009",
+            "nombre" : "Jenny Bazan Robles",
+            "nota" : 15
+        },
+        {
+            "codigo" : "EST0010",
+            "nombre" : "Gerson Obregon Leiva",
+            "nota" : 19
+        },
+        {
+            "codigo" : "EST0011",
+            "nombre" : "Eddie Santiago Ruiz",
+            "nota" : 16
+        },
+        {
+            "codigo" : "EST0012",
+            "nombre" : "Gustavo Mego Diaz",
+            "nota" : 13
+        },
+        {
+            "codigo" : "EST0013",
+            "nombre" : "Erika Delgado Torres",
+            "nota" : 18
+        },
+        {
+            "codigo" : "EST0014",
+            "nombre" : "Andrea Torres Iglesias",
+            "nota" : 14
+        },
+        {
+            "codigo" : "EST0015",
+            "nombre" : "Rosa Del Carpio Flores",
+            "nota" : 12
+        },
+        {
+            "codigo" : "EST0016",
+            "nombre" : "Silvana Diaz Osores",
+            "nota" : 10
+        },
+        {
+            "codigo" : "EST0017",
+            "nombre" : "Ana Maria Praderas del Rio",
+            "nota" : 19
+        },
+        {
+            "codigo" : "EST0018",
+            "nombre" : "Juan Pablo Genoves Solaris",
+            "nota" : 13
+        },
+        {
+            "codigo" : "EST0019",
+            "nombre" : "Jorge Frias Malpartida",
+            "nota" : 12
+        },
+        {
+            "codigo" : "EST0020",
+            "nombre" : "Elizabeth Puente Ramos",
+            "nota" : 18
+        },
+        {
+            "codigo" : "EST0021",
+            "nombre" : "Iris Anicama Zamora",
+            "nota" : 12
+        },
+        {
+            "codigo" : "EST0022",
+            "nombre" : "Eulalia Pascual Ubillus",
+            "nota" : 19
+        },
+        {
+            "codigo" : "EST0023",
+            "nombre" : "Dante Sanchez Egocheaga",
+            "nota" : 18
+        }
+]};
+
+function listadoEstudiantes(){
+    let rset=document.getElementById("resultados"),
+        html=`<table>
+                <tr>
+                    <th>Código</th>
+                    <th>Nombre</th>
+                    <th>Nota</th>
+                </tr>`
+    for(let i=0; i<objeto.length; i++){
+        html += `<tr>
+                    <td>${objeto.estudiantes[i].codigo}</td>
+                    <td>${objeto.estudiantes[i].nombre}</td>
+                    <td>${objeto.estudiantes[i].nota}</td>
+                </tr>`
     }
-];
+    html += `</table>`
+    rset.innerHTML = html
+}
+/*promedioNotas
+mayorNota
+menorNota*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
