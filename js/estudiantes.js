d969b610720b7ec7332c50be06cf5f5b1cea7257
Maycol Zambrano <maycol_630@hotmail.com>
2018-07-17 19:52:29 -0500

    Agregando funcionalidades adicionales (Ordenamiento asc/desc, fotos, separando notas mayores y menores del promedio)

--- js/estudiantes.js
+++ js/estudiantes.js
@@ -32,7 +32,7 @@ var objeto = {
         },
         {
             "codigo" : "EST0007",
-            "nombre" : "Ricardo More Gallegos",
+            "nombre" : "Esteban More Gallegos",
             "nota" : 16
         },
         {
@@ -57,33 +57,99 @@ var titulo = document.getElementById("titulo"),
     total = objeto.estudiantes.length,
     
 MyApp = {
-        listadoEstudiantes : function(){
+        detalleInicial : function(){
+            let html = ''
+            for(let i=0; i<total; i++){
+                html += `<img src="images/${objeto.estudiantes[i].codigo}.jpg" title="${objeto.estudiantes[i].nombre}" class="faces" />`
+            }
+            titulo.innerHTML = "Resultados de Evaluación"
+            resultado.innerHTML = `Este examen se realizó a un total de <strong>${total} estudiantes</strong>, a continuación seleccione las opciones que desea consultar en el menú superior. <br> ${html}`
+        },
+        ordernarObjeto : function(param1, param2){
+            objeto.estudiantes.sort(function (a, b){
+                if(param2=='asc'){ //asc Ascendente
+                    return (b[param1] < a[param1])
+                }else{ //desc Descendente
+                    return (b[param1] > a[param1])
+                }
+            })  
+        },
+        listadoEstudiantes : function(campo, orden){
             let html= `<table class="tbl_resultados">
                         <tr>
+                            <th width="10%">Foto</th>
                             <th width="20%">Código</th>
-                            <th width="60%">Nombre</th>
+                            <th width="50%">Nombre</th>
                             <th width="20%">Nota</th>
                         </tr>`
+            this.ordernarObjeto(campo,orden)
             for(let i=0; i<total; i++){
                 html += `<tr>
+                            <td align="center">
+                                <img src="images/${objeto.estudiantes[i].codigo}.jpg" />
+                            </td>
                             <td align="center">${objeto.estudiantes[i].codigo}</td>
                             <td align="left">${objeto.estudiantes[i].nombre}</td>
                             <td align="center">${objeto.estudiantes[i].nota}</td>
                         </tr>`
             }
-            html += `</table>`
+            html += `<tr>
+                            <td align="center">-</td>
+                            <td align="center">
+                                <a href="#" onclick="MyApp.listadoEstudiantes('codigo','asc')">ASC</a> | 
+                                <a href="#" onclick="MyApp.listadoEstudiantes('codigo','desc')">DESC</a>
+                            </td>
+                            <td align="center">
+                                <a href="#" onclick="MyApp.listadoEstudiantes('nombre','asc')">ASC</a> | 
+                                <a href="#" onclick="MyApp.listadoEstudiantes('nombre','desc')">DESC</a>
+                            </td>
+                            <td align="center">
+                                <a href="#" onclick="MyApp.listadoEstudiantes('nota','asc')">ASC</a> | 
+                                <a href="#" onclick="MyApp.listadoEstudiantes('nota','desc')">DESC</a>
+                            </td>
+                        </tr>
+                    </table>`
             titulo.innerHTML = "Listado de Estudiantes"
             resultado.innerHTML = html
         },
         promedioNotas : function(){
             let acumulador=0,
-                promedio=0
+                promedio=0,
+                mayores='',
+                menores=''
             for(let i=0; i<total; i++){
                 acumulador += parseInt(objeto.estudiantes[i].nota)
             }
             promedio = acumulador / total
-            titulo.innerHTML = "Nota Promedio"
-            resultado.innerHTML = `Nota: <strong>${promedio}</strong>`
+            promedio = promedio.toFixed()
+            this.ordernarObjeto('nota','desc')
+            for(let i=0; i<total; i++){
+                if(objeto.estudiantes[i].nota>=promedio){
+                    mayores += `
+                        Foto:<br> <img src="images/${objeto.estudiantes[i].codigo}.jpg" class="foto" /><br>
+                        Codigo: <strong>${objeto.estudiantes[i].codigo}</strong><br>
+                        Nombre: <strong>${objeto.estudiantes[i].nombre}</strong><br>
+                        Nota: <strong>${objeto.estudiantes[i].nota}</strong><hr>`
+                }else{
+                    menores += `
+                        Foto:<br> <img src="images/${objeto.estudiantes[i].codigo}.jpg" class="foto" /><br>
+                        Codigo: <strong>${objeto.estudiantes[i].codigo}</strong><br>
+                        Nombre: <strong>${objeto.estudiantes[i].nombre}</strong><br>
+                        Nota: <strong>${objeto.estudiantes[i].nota}</strong><hr>`
+                }
+            }
+            
+            titulo.innerHTML = `Nota Promedio ${promedio}`
+            resultado.innerHTML = `<table class="tbl_resultados">
+                                        <tr>
+                                            <th>Notas Menores al Promedio</th>                                            
+                                            <th>Notas Mayores o Igual al Promedio</th>
+                                        </tr>
+                                        <tr>
+                                            <td>${menores}</td>                                            
+                                            <td>${mayores}</td>
+                                        </tr>
+                                    </table>`
         },
         mayorNota : function(){
             let mayor=-Infinity,
@@ -96,6 +162,7 @@ MyApp = {
             }
             titulo.innerHTML = "Mayor Nota"
             resultado.innerHTML = `
+                        Foto:<br> <img src="images/${objeto.estudiantes[posicion].codigo}.jpg" class="foto" /><br>
                         Codigo: <strong>${objeto.estudiantes[posicion].codigo}</strong><br>
                         Nombre: <strong>${objeto.estudiantes[posicion].nombre}</strong><br>
                         Nota: <strong>${mayor}</strong>`
@@ -111,6 +178,7 @@ MyApp = {
             }
             titulo.innerHTML = "Menor Nota"
             resultado.innerHTML = `
+                        Foto:<br> <img src="images/${objeto.estudiantes[posicion].codigo}.jpg" class="foto" /><br>
                         Codigo: <strong>${objeto.estudiantes[posicion].codigo}</strong><br>
                         Nombre: <strong>${objeto.estudiantes[posicion].nombre}</strong><br>
                         Nota: <strong>${menor}</strong>`
@@ -118,7 +186,7 @@ MyApp = {
     }
 
 
-MyApp.listadoEstudiantes()
+MyApp.detalleInicial()
 
 
 
